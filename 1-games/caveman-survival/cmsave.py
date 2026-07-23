237b39a4fce0667dbac5c37641502e4afc2c4e48
Noriel Guillermo Cezar <norielg.cezar88@gmail.com>
2026-07-23 15:59:19 +0800

    Create cmsave.py

---
+++ 1-games/caveman-survival/cmsave.py
@@ -0,0 +1,26 @@
+class cmsave:
+ def __init__(self,player,hp,mhp,en,men,hg,mhg,day,wea,fr,w,x,c,po,pp,fruit,wood,sap,flint,stick,glue,spear,inv):
+  self.player=player
+  self.hp=hp
+  self.mhp=mhp
+  self.en=en
+  self.men=men
+  self.hg=hg
+  self.mhg=mhg
+  self.day=day
+  self.wea=wea
+  self.fr=fr
+  self.w=w
+  self.x=x
+  self.c=c
+  self.po=po
+  self.pp=pp
+  self.fruit=fruit
+  self.wood=wood
+  self.sap=sap
+  self.flint=flint
+  self.stick=stick
+  self.glue=glue
+  self.spear=spear
+  self.inv=inv
+cmsave1=cmsave("???",4,4,3,3,1,2,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,[])
